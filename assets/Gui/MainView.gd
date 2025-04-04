8594556a61d9207bb9eb724505b1a89f3a0615a4
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-04 22:40:00 +0200

    Chg: Complete redesign of Main Game UI Chg: Seperate them as Panels Upd: Ovrhaul Project Theme Resource Upd: Update many componentnodes

--- assets/Gui/MainView.gd
+++ assets/Gui/MainView.gd
@@ -1,11 +1,12 @@
 extends Control
 
 @onready var start_options = %StartOptions
-@onready var container_multiplayer_options = %ContainerMultiplayerOptions
-@onready var multiplayer_host = %MultiplayerHost
-@onready var multiplayer_client = %MultiplayerClient
+#@onready var container_multiplayer_options = %ContainerMultiplayerOptions
+#@onready var multiplayer_host = %MultiplayerHost
+#@onready var multiplayer_client = %MultiplayerClient
 
-
+@export_subgroup("SubPanels", "MainMenu")
+@export var MainMenuSubViewPanes : Dictionary[String, NodePath] = {}
 
 # var _node_name = "Main"
 ## Seperate MeinMenu in own ViewScene
@@ -13,6 +14,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	preload("res://assets/ball/ball_body.tscn")
 	pass # Replace with function body.
 
 
@@ -23,21 +25,23 @@ func _process(_delta):
 
 func _on_button_start_hot_seat_pressed():
 	get_tree().change_scene_to_file("res://scenes/Pong_40.tscn")
+	
 
 
 func _on_button_multiplayer_options_pressed():
-	container_multiplayer_options.visible = !container_multiplayer_options.visible
+	pass
 
 
 func _on_button_join_server_pressed():
-	multiplayer_client.visible = true
-	multiplayer_host.visible = false
+	#multiplayer_client.visible = true
+	#multiplayer_host.visible = false
+	pass
 
 
 func _on_button_create_server_pressed():
-	multiplayer_host.visible = true
-	multiplayer_client.visible = false
-
+	#multiplayer_host.visible = true
+	#multiplayer_client.visible = false
+	pass
 
 func _on_button_start_pressed():
 	start_options.visible = !start_options.visible
