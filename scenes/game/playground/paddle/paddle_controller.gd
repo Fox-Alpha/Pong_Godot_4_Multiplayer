03daf87154ed9b51b915c17452f88f406fcc520f
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-06 13:43:40 +0200

    Chg: Refactor some behaviors ===: Setting Color ===: Setting Position ===: Input Handling

--- scenes/game/playground/paddle/paddle_controller.gd
+++ scenes/game/playground/paddle/paddle_controller.gd
@@ -1,28 +1,33 @@
 extends CharacterBody2D
 
+const BLUEPLAYER : Color = Color.DARK_BLUE
+const REDPLAYER : Color = Color.DARK_RED
+const DEFAULTCOLOR : Color = Color.WHITE
 
-@export_range(100.0,1000.0,10.0) var SPEED = 300.0
+@export_range(100.0,1000.0,10.0) var SPEED = 600.0
+@export_enum("PLAYER_1", "PLAYER_2") var PlayerPaddle = 0
+@export_color_no_alpha var playercolor = DEFAULTCOLOR
 
-@export_enum("Player_1", "Player_2") var PlayerPaddle = 0
+var PlayerLeftPosition : Vector2 = Vector2(10, get_viewport_rect().size.y/2)
+var PlayerRightPosition : Vector2 = Vector2(get_viewport_rect().size.x-10, get_viewport_rect().size.y/2)
 
-@export_color_no_alpha var playercolor
 
 func _ready():
 	Game.connect("New_Game_Started", _reset_position, CONNECT_DEFERRED)
 	Game.connect("Next_Round_Started", _reset_position, CONNECT_DEFERRED)
 
+	if playercolor == DEFAULTCOLOR:
+		playercolor = BLUEPLAYER if playercolor == DEFAULTCOLOR else REDPLAYER
 
-func _process(_delta):
-	
-	pass
-
+	$Paddle.self_modulate = Game.get_playercolor(PlayerPaddle)
 
 func _physics_process(delta):
 	var direction
 	match PlayerPaddle:
-		0:
-			direction = Vector2(0, Input.get_action_strength("p1_down") - Input.get_action_strength("p1_up"))
-		1:
+		PlayerPaddle.PLAYER_1:
+			direction = Input.get_vector("", "", "p1_down", "p1_up")
+			#direction = Vector2(0, Input.get_action_strength("p1_down") - Input.get_action_strength("p1_up"))
+		PlayerPaddle.PLAYER_2:
 			direction = Vector2(0, Input.get_action_strength("p2_down") - Input.get_action_strength("p2_up"))
 
 	if(direction):
@@ -31,13 +36,14 @@ func _physics_process(delta):
 		move_and_collide(velocity)
 
 func _reset_position():
-	var screensize = get_viewport_rect()
-	
+	#var screensize = get_viewport_rect()
 	# Paddle Positionen
 	match PlayerPaddle:
 		0:
-			position = Vector2(10, screensize.size.y/2)
+			#position = Vector2(10, screensize.size.y/2)
+			position = PlayerLeftPosition
 		1:
-			position = Vector2(screensize.size.x-10, screensize.size.y/2)
-	var col = Game.get_playercolor(PlayerPaddle)
-	$Paddle.self_modulate = col
+			#position = Vector2(screensize.size.x-10, screensize.size.y/2)
+			position = PlayerRightPosition
+	#var col = Game.get_playercolor(PlayerPaddle)
+	#$Paddle.self_modulate = col
