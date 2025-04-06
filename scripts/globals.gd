58370e9605361b9406bfbb62e391bad0ae092ff1
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-06 11:35:31 +0200

    Rem: Unused code

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,10 +1,8 @@
 extends Node
 
-#class_name Game
-
-var MaxScore : int = 1
-var currentround :int = 1
-var maxrounds :int
+var MaxScore : int = 10
+var currentround : int = 1
+var maxrounds : int = 3
 
 var hasGamestartet : bool = false
 var waitForNextRound : bool = false
@@ -61,32 +59,21 @@ func _check_win_state():
 
 func update_player_dict(p1:String, p2:String,score:int = 10,rounds:int = 3, colors:Array = [Color.DARK_BLUE, Color.ORANGE_RED]):
 	playerdic.clear()
-#
 	playerdic = {"player1":p1, "player2":p2, "rounds":rounds, "score": score, "colors":colors}
-#
+
 	for r in range(1, rounds+1):
 		var roundname = "round_{rnd}".format({"rnd":str(r)})
 		playerdic[roundname] = {"p1": 0, "p2":0, "won": "NONE"}
-#	print_debug(playerdic)
 	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	connect("Left_Player_Scored", ScoreLeft, CONNECT_DEFERRED)
-#	connect("Right_Player_Scored", ScoreRight, CONNECT_DEFERRED)
-	connect("Game_Is_over", _Game_Is_over, CONNECT_DEFERRED)
-#
+	Game_Is_over.connect( _Game_Is_over, CONNECT_DEFERRED)
+
 
 func _Game_Is_over(ply):
 	var roundname = "round_{rnd}".format({"rnd":str(currentround)})
-#	var won
-	
-#	match ply:
-#		"p1":
-#			won = 1
-#		"p2":
-#			won = 2
-	
+
 	playerdic[roundname] = {"p1": p1_score, "p2":p2_score, "won": ply}
 	currentround += 1
 	pass
@@ -96,14 +83,3 @@ func get_playercolor(ply:int) -> Color:
 	#var col = playerdic["colors"][ply]
 	randomize()
 	return Color.from_rgba8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255))# col
-
-#func ScoreLeft(score : int):
-#	pass # Replace with function body.
-#
-#
-#func ScoreRight(score : int):
-#	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta):
-#	pass
