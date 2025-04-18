31691675bca23c0b9a4eabb734ac72e5dc21a53b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-18 11:22:05 +0200

    Rem: Positioning on Screencenter ===: Unused Code

--- scenes/debug/debug.gd
+++ scenes/debug/debug.gd
@@ -9,15 +9,7 @@ func _ready() -> void:
 	%ResetButton.pressed.connect(func(): Game.Game_Reseted.emit())
 	Game.DebugControl = self
 	SetDebugText.connect(_SetDebugText)
-	var screensize := get_viewport_rect()
-	var bottomcenter := Vector2( screensize.size.x / 2.0, screensize.size.y)
-	#position = bottomcenter
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
 
 func _SetDebugText(DbgText : String) -> void:
 	debug.text = DbgText
