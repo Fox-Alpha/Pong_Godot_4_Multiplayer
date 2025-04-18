695e1c000adb86349e45fc8c5dad855c64b3955f
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-18 12:08:43 +0200

    Add: Manager for Score Logics

---
+++ scripts/gamelogic/score_manager.gd
@@ -0,0 +1,78 @@
+extends ManagerBaseClass
+class_name ScoreManager
+
+#region Variables
+var currentround : int = 1
+var maxrounds : int = 3
+#endregion
+
+#region SCORES
+var MaxScore : int = 10
+var p1_score :int = 0 :
+	set (value):
+		p1_score = value
+	get:
+		return p1_score
+var p2_score : int = 0 :
+	set (value):
+		p2_score = value
+	get:
+		return p2_score
+#endregion
+
+var playerdic : Dictionary = {"player1":"", "player2":"", "Rounds":1}
+
+#region SIGNALS
+signal Left_Player_Scored(score : int)
+signal Right_Player_Scored(score : int)
+#endregion
+
+func _ready() -> void:
+	print("ScoreManager => _ready()")
+	super()
+	Left_Player_Scored.connect(_Left_Player_Scored, CONNECT_DEFERRED )
+	Right_Player_Scored.connect(_Right_Player_Scored, CONNECT_DEFERRED )
+
+
+func _enter_tree() -> void:
+	print("ScoreManager => _enter_tree()")
+	Game.Register_SCORE_Manager.emit(self.get_instance_id())
+	pass
+
+#####
+
+func _Left_Player_Scored(scorevalue : int = 1) -> void:
+	p1_score += scorevalue
+	if _Check_Score_State(p1_score):
+		pass
+
+
+func _Right_Player_Scored(scorevalue : int = 1) -> void:
+	p2_score += scorevalue
+	if _Check_Score_State(p2_score):
+		pass
+
+#####
+
+func _Check_Round_Win_State():
+	if(MaxScore == p1_score):
+		pass # emit_signal("Game_Is_over", playerdic["player1"])
+	elif(MaxScore == p2_score):
+		pass # emit_signal("Game_Is_over", playerdic["player2"])
+
+
+func _Check_Max_Round_State() -> bool:
+	return currentround == maxrounds+1
+
+
+func _Check_Score_State(score) -> bool:
+	return MaxScore == score
+
+#####
+
+#func _check_next_round() -> bool:
+	#if _check_score_state(p1_score) or _check_score_state(p2_score):
+		#Game.Game_Prepare_Next_Round.emit()
+		#return true
+	#else:
+		#return false
