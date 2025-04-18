6190cee4a8bd23f42f968fae70c05041458e00da
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-18 11:37:56 +0200

    Chg: Using own method for randomizing direction at Startpoint Add: Logic fo GameState changes

--- scenes/game/playground/ball/ball.gd
+++ scenes/game/playground/ball/ball.gd
@@ -4,23 +4,46 @@ const SPEED : float = 200.0
 var _speed : float = 0.0
 var ballspeed : Vector2 = Vector2.ZERO
 
-var dbg : Control = null
-
+var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready():
-	Game.connect("New_Game_Started", reset, CONNECT_DEFERRED)
-	Game.connect("Next_Round_Started", reset, CONNECT_DEFERRED)
-	
-	randomize()
-	velocity.x = [-1,1][randi() % 2]
-	velocity.y = [-0.8, 0.8][randi() % 2]
+	print("Ball Szene => _ready()")
+	Game.Game_State_Changed.connect(_Game_State_Has_Changed, CONNECT_DEFERRED)
 
-	_speed = SPEED
 
+func _Game_State_Has_Changed(new_gs : Game.GameStates) -> void:
+	print("Ball Szene => _Game_State_Has_Changed(GS:%s)" % Game.GameStates.keys()[new_gs])
+	match new_gs:
+		Game.GameStates.GAMEISLOADING:
+			pass
+		Game.GameStates.GAMEINITIALIZING:
+			_speed = SPEED
+			pass
+		Game.GameStates.GAMEINITIALIZED:
+			_Connect_Signals()
+			velocity = _randomize_ball_direction()
+			pass
+		Game.GameStates.GAMEWAITFORSTART:
+			pass
+		Game.GameStates.GAMEISSTARTED:
+			pass
+		Game.GameStates.GAMEOVER:
+			pass
+
+
+func _Connect_Signals():
+	#Game.Game_Prepare_Round.connect(reset_ball_position, CONNECT_DEFERRED)
+	#Game.Game_Prepare_Next_Round.connect(reset_ball_position, CONNECT_DEFERRED)
+	pass
+
+
+func _enter_tree() -> void:
+	print("Ball Szene => _enter_tree()")
 
 func _physics_process(delta):
-	if(!Game.hasGamestartet):
+	if Game.GameState != Game.GameStates.GAMEISSTARTED:
 		return
+
 	_speed = clampf(_speed * 1.1, SPEED, 700.0)
 	ballspeed =  velocity * delta * _speed
 	ballspeed = ballspeed.clamp(Vector2(-15, -15), Vector2(15, 15))
@@ -28,22 +51,28 @@ func _physics_process(delta):
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
 	if Game.DebugControl:
-		Game.DebugControl.SetDebugText.emit(str(ballspeed))
+		Game.DebugControl.SetDebugText.emit("Ball Vector (%.3f / %.3f - Speed %d)" % [ballspeed.x, ballspeed.y, _speed]) #str(ballspeed)
 
 
-func reset():
+func reset_ball_position():
 	position =  get_viewport_rect().get_center()
-	randomize()
-	velocity.x = [-1,1][randi() % 2]
-	velocity.y = [-0.8, 0.8][randi() % 2]
+	velocity = _randomize_ball_direction()
 	_speed = SPEED
 
 
+func _randomize_ball_direction() -> Vector2:
+	var direction : Vector2 = Vector2.ZERO
+	rng.randomize()
+	direction.x = [-1,1][rng.randi() % 2]
+	direction.y = [-0.8, 0.8][rng.randi() % 2]
+	return direction
+
+
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	if(!Game.hasGamestartet):
+	if(!Game.GameStates.GAMEISSTARTED):
 		return
+
 	if(position.x < 0):
-		Game.emit_signal("Right_Player_Scored", 1)
+		Game.Scr_Manager.Right_Player_Scored.emit(1)
 	else:
-		Game.emit_signal("Left_Player_Scored", 1)
-#	reset()
+		Game.Scr_Manager.Left_Player_Scored.emit(1)
