05496db6e9b1c91bf4928a1fa50c94beb908f187
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-04-18 12:10:35 +0200

    Mve: Score logic to score Manager Chg: Refactor score display Add: Logic fo GameState changes

--- scenes/game/ingame_ui/ui.gd
+++ scenes/game/ingame_ui/ui.gd
@@ -1,64 +1,60 @@
 extends Control
 
-@onready var _game_message_container: PanelContainer = %PanelContainer_GameMessages
-@onready var _game_message_label: Label = %GameMessageLabel
-
-
-const ANYKEYMESSAGE : String ="Press Any Key to start"
-const GAMEOVER : String ="Game Over"
-const NEXTROUND : String ="Next Round"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Game.Game_State_Changed.connect(_Game_State_Has_Changed)
+
+
+func _Game_State_Has_Changed(new_gs : Game.GameStates) -> void:
+	print("UI Szene ==> _Game_State_Has_Changed(GS:%s)" % Game.GameStates.keys()[new_gs])
+	match new_gs:
+		Game.GameStates.GAMEISLOADING:
+			pass
+		Game.GameStates.GAMEINITIALIZING:
+			pass
+		Game.GameStates.GAMEINITIALIZED:
+			_Connect_Signals()
+			_Game_Initialized()
+			pass
+		Game.GameStates.GAMEWAITFORSTART:
+			#Show Center Message ?
+			pass
+		Game.GameStates.GAMEISSTARTED:
+			Next_Round_Started()
+			pass
+		Game.GameStates.GAMEOVER:
+			pass
+
+
+func _Connect_Signals() -> void:
 	# Global Signals
-	Game.Left_Player_Scored.connect(ScoreLeft, CONNECT_DEFERRED)
-	Game.Right_Player_Scored.connect(ScoreRight, CONNECT_DEFERRED)
-	Game.Game_Is_over.connect(Game_Is_over, CONNECT_DEFERRED)
-	Game.New_Game_Started.connect(New_Game_Started, CONNECT_DEFERRED)
-	Game.Next_Round_Started.connect(Next_Round_Started, CONNECT_DEFERRED)
+	Game.Scr_Manager.Left_Player_Scored.connect(ScoreLeft, CONNECT_DEFERRED)
+	Game.Scr_Manager.Right_Player_Scored.connect(ScoreRight, CONNECT_DEFERRED)
 	######
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta):
-#	pass
-
-func New_Game_Started():
-	%PlayerScoreLeft.text = "00000"
-	%PlayerScoreRight.text = "00000"
+func _Game_Initialized():
+	%PlayerScoreLeft.text = "%0*d" % [3,Game.Scr_Manager.p1_score]
+	%PlayerScoreRight.text = "%0*d" % [3,Game.Scr_Manager.p2_score]
 
 	var p : String = ""
-	p = Game.playerdic["player1"]
+	#p = Game.playerdic["player1"]
 	%LabelP1Name.text = p if (!p.is_empty()) else "P1"
-	
+
 	p = ""
-	p = Game.playerdic["player2"]
+	#p = Game.playerdic["player2"]
 	%LabelP2Name.text = p if (!p.is_empty()) else "P2"
 
 
 func Next_Round_Started():
-	_game_message_container.visible = false
-
-
-func ScoreLeft(score : int):
-	Game.p1_score += score
-	%PlayerScoreLeft.text = str(Game.p1_score)
-	#%StartLabel.visible = true
-
-
-func ScoreRight(score : int):
-	Game.p2_score += score
-	%PlayerScoreRight.text = str(Game.p2_score)
-	#%StartLabel.visible = true
-
-
-func Game_Is_over(player : String):
-	#%WinLabel.text = "GameOver %s has won" % player
-	#%WinLabel.visible = true
-	ShowGameMessage("GameOver %s has won" % player)
-
-func ShowGameMessage(message : String) -> void:
-	_game_message_label.text = message
-	_game_message_container.visible = true
+	%PlayerScoreLeft.text = "%0*d" % [3,Game.Scr_Manager.p1_score]
+	%PlayerScoreRight.text = "%0*d" % [3,Game.Scr_Manager.p2_score]
 	pass
+
+
+func ScoreLeft(_score : int):
+	%PlayerScoreLeft.text = "%0*d" % [3,Game.Scr_Manager.p1_score]
+
+
+func ScoreRight(_score : int):
+	%PlayerScoreRight.text = "%0*d" % [3,Game.Scr_Manager.p2_score]
